9ec5a2a977834088bd8186d40e66f36dc8e45a4f
Boorsoft <azim.akmatov1@gmail.com>
2020-05-04 09:41:40 +0000

    Student model expanded

--- restapi/models.py
+++ restapi/models.py
@@ -13,7 +13,7 @@ class Teacher(models.Model):
   first_name = models.CharField(max_length=50)
   middle_name = models.CharField(max_length=50)
   subject = models.ManyToManyField(Subject, related_name="subject")
-
+    
   def __str__(self):
     return self.last_name
 
@@ -21,13 +21,17 @@ class Question(models.Model):
   question_id = models.AutoField(primary_key=True)
   question_body = models.CharField(max_length=300)
 
+  def __str__(self):
+    return self.question_body
+
 class Student(models.Model):
   student_id = models.AutoField(primary_key=True)
-  student = models.CharField(max_length=200)
-  subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+  username = models.CharField(max_length=50, null=True)
+  password = models.CharField(max_length=100, null=True)
+  subject = models.ManyToManyField(Subject)
 
   def __str__(self):
-    return self.student
+    return self.username
   
 class Answer(models.Model):
   answer_id = models.AutoField(primary_key=True)
