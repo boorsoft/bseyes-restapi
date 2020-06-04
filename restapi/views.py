f4d7cbed876ba4271841978c9a2c32546a103731
akmatoff <azim.akmatov1@gmail.com>
2020-06-04 10:20:02 +0600

    Showing results in PDF implemented

--- restapi/views.py
+++ restapi/views.py
@@ -1,7 +1,7 @@
 from django.http import FileResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from .models import Answer, Subject, Teacher
-from reportlab.platypus import SimpleDocTemplate, Paragraph, PageBreak
+from reportlab.platypus import SimpleDocTemplate, Paragraph, PageBreak, Spacer
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
@@ -37,7 +37,7 @@ def teachers(request, id):
 # Answers 
 def result(request, subject_id, teacher_id):
   # fetch data
-  answers = list(Answer.objects.all())
+  answers = Answer.objects.filter(subject=subject_id, teacher=teacher_id)
 
   pdf_buffer = BytesIO()
   font = "static/fonts/OpenSans-Light.ttf"
@@ -59,24 +59,47 @@ def result(request, subject_id, teacher_id):
   title_style.fontName = 'OpenSansBold'
   title_style.fontSize = 12
 
+  heading_style = style['Heading4']
+  heading_style.fontName = 'OpenSansBold'
+
   page_break = PageBreak()
-  title = Paragraph("Анкетирование", title_style)
-  
+  space = Spacer(1, 20)
 
-  content.append(title)
+  q_num = 1
 
-  i = 1
-  n = 0
-
+  # 1 answer = 1 page
   for answer in answers:
-    question = Paragraph('Вопрос № ' + str(i), body_style)
+    course = Paragraph('<font name="OpenSansBold">Курс</font> ' + answer.subject.sub_name, body_style)
+    teacher = Paragraph('<font name="OpenSansBold">Преподаватель</font> '+ answer.teacher.first_name + ' ' + answer.teacher.last_name, body_style)
+    info = Paragraph('1 - Абсолютно не согласен, 5 - Полностью согласен', body_style)
+    content.append(course)
+    content.append(teacher)
+    content.append(space)
+    content.append(info)
+    content.append(space)
+
     rates = answer.rate.split(",")
-    print(rates)
-    content.append(question)
-    i += 1
+
+    for question in answer.question.all():
+      question = Paragraph(str(q_num) + '. ' + question.question_body, body_style)
+      content.append(question)
+
+      for rate in rates:
+        rate = Paragraph(rate, body_style)
+        content.append(rate)
+        break
+
+      q_num += 1
+
+    comment_heading = Paragraph('Комментарий: ', heading_style)
+    comment = Paragraph(answer.comment, body_style)
+    content.append(space)
+    content.append(comment_heading)
+    content.append(comment)
+    q_num = 1
+    content.append(page_break)
 
   results_doc.build(content)
-  print(answers)
   pdf_buffer.seek(0)
   if request.user.is_authenticated:
     return FileResponse(pdf_buffer, as_attachment=False, filename='results.pdf')
